ce60f3168c5b08df2c8a211d11c19e59c3084dca
FabianDiazTamayo <fdiaztamayo@gmail.com>
2026-06-29 10:41:46 +0200

    Remove debug_jobs.py (security cleanup)

--- py_updates/qfield_service/debug_jobs.py
+++
@@ -1,7 +0,0 @@
-from qfieldcloud_sdk import sdk
-client = sdk.Client(url="https://app.qfield.cloud/api/v1/")
-client.login("***REMOVED***", "***REMOVED***")
-
-jobs = client.list_jobs("f829bc38-1f8c-4ea9-a891-521b0f67d58b")
-for j in jobs[:10]:
-    print(j.get("type"), "|", j.get("status"), "|", j.get("id"))
